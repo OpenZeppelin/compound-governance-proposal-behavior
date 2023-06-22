40bb6a1e829af534df3886160277ebdcd2fbd235
Morgan Weaver <morganjweaver@gmail.com>
2023-06-22 19:43:23 +0100

    test passing
    :

--- test/Simulation.t.sol
+++ test/Simulation.t.sol
@@ -8,13 +8,14 @@ import "./Helpers.sol";
 import "../src/IGovernorBravo.sol";
 import "../src/Timelock.sol";
 import "../src/IComp.sol";
+import { AggregatorV3Interface } from "chainlink/interfaces/AggregatorV3Interface.sol";
 
 // @dev These tests were developed for block 17535430 (add with `--fork-block-number 17535430`)
 contract SimulationTest is Helpers {
     // Constants
     uint256 constant proposalTH = 25000000000000000000000; // 25,000 COMP
-    uint256 constant paymentAmount = 29455081001472800000000 // 1 Million in COMP based on 7-day 35.95 MA
-    
+    uint256 constant paymentAmount = 29455081001472800000000; // 1 Million in COMP based on 7-day 35.95 MA
+    // address public COMP_USD_priceFeed = 0xdbd020caef83efd542f4de03e3cf0c28a4428bd5;
     // Contracts
     IGovernorBravo governorBravo = IGovernorBravo(0xc0Da02939E1441F497fd74F78cE7Decb17B66529);
     Timelock timelock = Timelock(payable(0x6d903f6003cca6255D85CcA4D3B5E5146dC33925));
@@ -52,7 +53,7 @@ contract SimulationTest is Helpers {
         comp.delegate(Genesis);
         
         // Delegate Binance votes to Binance
-        vm.prank(Binance);s
+        vm.prank(Binance);
         comp.delegate(Binance);
         
         // Voting power is not instant
@@ -87,18 +88,16 @@ contract SimulationTest is Helpers {
         calldatas[0] = abi.encode(Multisig, paymentAmount);
 
         // Description
-        string memory description = "# OpenZeppelin Security Partnership - 2023 Q3 Compensation ## Background Starting on Dec 21st \n"
-      "2021 \n"
-      OpenZeppelin was [selected](https://compound.finance/governance/proposals/76) to offer the Compound DAO security services including continuous audit
-      security advisory
-      and monitoring. At the start of every quarter
-      OpenZeppelin creates a proposal to perform the next service fee payment. ## Compensation Structure We receive our quarterly payments in a lump-sum of COMP. Based on the last week's average price
-      this would be $42.34 per COMP for a total quarterly payment of 23
-      617 COMP equaling $1M per the original agreement. This COMP will be transferred from the Timelock's existing balance. More detail in this [forum post](https://www.comp.xyz/t/openzeppelin-security-updates-for-q1-2023-q2-compensation-proposal/4163). By approving this proposal
-      you agree that any services provided by OpenZeppelin shall be governed by the [Terms of Service available here](https://docs.google.com/document/d/1eDcvirf0bdzpjIkfxQHm7fcV05O1IcLXL-Dl-U52Y4k/edit?usp=sharing)";
+        string memory description = "# OpenZeppelin Security Partnership - 2023 Q3 Compensation ## Background Starting on Dec 21st, 2021 \n"
+      "OpenZeppelin was [selected](https://compound.finance/governance/proposals/76) to offer the Compound DAO security services including continuous audit"
+      "security advisory and monitoring. At the start of every quarter OpenZeppelin creates a proposal to perform the next service fee payment."
+      " ## Compensation Structure We receive our quarterly payments in a lump-sum of COMP. Based on the last week's average price"
+      "this would be $35.95 per COMP for a total quarterly payment of 29,455 COMP equaling $1M per the original agreement. This COMP will be transferred "
+      "from the Timelock's existing balance. More detail in this [forum post](https://www.comp.xyz/t/openzeppelin-security-updates-for-q1-2023-q2-compensation-proposal/4163)."
+      " By approving this proposal you agree that any services provided by OpenZeppelin shall be governed by the [Terms of Service available here](https://docs.google.com/document/d/1eDcvirf0bdzpjIkfxQHm7fcV05O1IcLXL-Dl-U52Y4k/edit?usp=sharing)";
 
         // We submit the proposal as the attacker
-        vm.prank(attacker);
+        vm.prank(OzProposer);
 
         uint256 proposalID = governorBravo.propose(
             targets,
@@ -111,7 +110,7 @@ contract SimulationTest is Helpers {
         return proposalID;
     }
 
-    function voteOnProposal(uint256 proposalID, uint8 fundVote, uint8 genesisVote, uint8 binanceVote) internal {
+    function voteOnProposal(uint256 proposalID, uint8 fundVote, uint8 genesisVote, uint8 binanceVote, uint8 benefactorVote) internal {
         // We increment the time based on the voting delay
         increaseBlockNumber(governorBravo.votingDelay() + 1);
 
@@ -125,112 +124,20 @@ contract SimulationTest is Helpers {
 
         vm.prank(Binance);
         governorBravo.castVote(proposalID, binanceVote);
-    }
 
-    function giveCompToAttackerAndDelegate(uint256 amount) internal {
-        // Benefactor transfers tokens to the attacker
         vm.prank(Benefactor);
-        comp.transfer(attacker, amount);
-        
-        // Attacker delegates voting power to themselves
-        vm.prank(attacker);
-        comp.delegate(attacker);
-
-        increaseBlockNumber(1000);
-
-        // Checks if the attacker now has voting power
-        assertEq(comp.getCurrentVotes(attacker) > 0, true);
+        governorBravo.castVote(proposalID, benefactorVote);
     }
 
-    function testNotReaching25kProposal() public {
-        // Without voting power, the attacker should not be able to create a proposal
-        vm.expectRevert("GovernorBravo::propose: proposer votes below proposal threshold");
-        createProposal();
+    function testPaymentProposal() public {
+        // Validate multisig COMP balance is 0
+        uint256 initialCompBalanceMultisig = comp.balanceOf(Multisig);
 
-        // Checks if attacker was somehow successful
-        assertEq(timelock.pendingAdmin() != attacker, true);
-        assertEq(timelock.admin() != attacker, true);
-    }
-
-    function testNotReaching400kProposal() public {
-        // Attacker needs more than `proposalThreshold` to pass a proposal
-        // Gets this from the Benefactor
-        giveCompToAttackerAndDelegate(proposalTH + 1);
-        
-        // Creates the proposal as the Attacker
-        uint256 proposalID = createProposal();
-        increaseBlockNumber(governorBravo.votingDelay() + 1);
-        
-        vm.startPrank(attacker);
-
-        // Attacker votes on their proposal
-        governorBravo.castVote(proposalID, 1);
-
-        // We increment the time based on the voting period
-        increaseBlockNumber(governorBravo.votingPeriod() + 1);
-
-        // Try to queue the proposal without any other vote but cannot reach the 400,000 COMP limit
-        vm.expectRevert("GovernorBravo::queue: proposal can only be queued if it is succeeded");
-        governorBravo.queue(proposalID);
-
-        vm.stopPrank();
-
-        // Asserts that the proposal was defeated
-        assertEq(governorBravo.state(proposalID), 3); // Defeated Proposal == 3
-
-        // Checks if the attacker was somehow successful
-        assertEq(timelock.pendingAdmin() != attacker, true);
-        assertEq(timelock.admin() != attacker, true);
-    }
-
-    function testReaching400kRejectedProposal() public {
-        // Attacker gets 400,000 COMP (4% of totalSupply) to have enough to
-        // pass the proposal when no one votes against
-        giveCompToAttackerAndDelegate(comp.totalSupply() * 4 / 100);
-        
         // Creates the proposal
         uint256 proposalID = createProposal();
         
-        // Other holders vote as abstain, abstain, against
-        voteOnProposal(proposalID, 2, 2, 0);
-
-        vm.startPrank(attacker);
-
-        // Attacker votes on their proposal
-        governorBravo.castVote(proposalID, 1);
-
-        // We increment the time based on the voting period
-        increaseBlockNumber(governorBravo.votingPeriod() + 1);
-
-        // Checks that the proposal was defeated
-        assertEq(governorBravo.state(proposalID), 3); // Defeated == 3
-
-        // Expects to revert when queuing it
-        vm.expectRevert("GovernorBravo::queue: proposal can only be queued if it is succeeded");
-        governorBravo.queue(proposalID);
-
-        vm.stopPrank();
-
-        // Checks if the attacker was somehow successful
-        assertEq(timelock.pendingAdmin() != attacker, true);
-        assertEq(timelock.admin() != attacker, true);
-    }
-
-    function test400kAttackerWithHelp() public {
-        // Attacker gets 400,000 COMP (4% of totalSupply) to have enough to
-        // pass the proposal when no one votes against
-        giveCompToAttackerAndDelegate(comp.totalSupply() * 4 / 100);
-        
-        // Creates the proposal
-        uint256 proposalID = createProposal();
-        
-        // Other hodlers vote as abstain, in favor, against
-        voteOnProposal(proposalID, 2, 1, 0);
-
-        vm.startPrank(attacker);
-
-        // Attacker votes on their proposal
-        governorBravo.castVote(proposalID, 1);
+        // Whales vote as in favor
+        voteOnProposal(proposalID, 1, 1, 1, 1);
 
         // We increment the time based on the voting period
         increaseBlockNumber(governorBravo.votingPeriod() + 1);
@@ -251,60 +158,58 @@ contract SimulationTest is Helpers {
         // Asserts it was executed (7)
         assertEq(governorBravo.state(proposalID), 7);
 
-        // Attacker accepts the admin role in the timelock
-        timelock.acceptAdmin();
-
-        vm.stopPrank();
+        // Validate multisig COMP balance is +29455 COMP
+        uint256 newCompBalanceMultisig = comp.balanceOf(Multisig);
 
         // Validates that the attacker is the new admin
-        assertEq(timelock.pendingAdmin(), address(0));
-        assertEq(timelock.admin(), attacker);
+        assertGt((newCompBalanceMultisig - initialCompBalanceMultisig), 29455 ether);
+        
     }
 
-    // This is the analogous test to the one provided
-    // by the disclosed bug
-    function testReaching400kPassedProposal() public {
-        // Attacker gets 400,000 COMP (4% of totalSupply) to have enough to
-        // pass the proposal when no one votes against
-        giveCompToAttackerAndDelegate(comp.totalSupply() * 4 / 100);
+    // // This is the analogous test to the one provided
+    // // by the disclosed bug
+    // function testReaching400kPassedProposal() public {
+    //     // Attacker gets 400,000 COMP (4% of totalSupply) to have enough to
+    //     // pass the proposal when no one votes against
+    //     giveCompToAttackerAndDelegate(comp.totalSupply() * 4 / 100);
         
-        // Attacker creates the proposal
-        uint256 proposalID = createProposal();
-        increaseBlockNumber(governorBravo.votingDelay() + 1);
+    //     // Attacker creates the proposal
+    //     uint256 proposalID = createProposal();
+    //     increaseBlockNumber(governorBravo.votingDelay() + 1);
         
-        vm.startPrank(attacker);
+    //     vm.startPrank(attacker);
 
-        // Attacker votes on their proposal
-        governorBravo.castVote(proposalID, 1);
+    //     // Attacker votes on their proposal
+    //     governorBravo.castVote(proposalID, 1);
 
-        // We increment the time based on the voting period
-        increaseBlockNumber(governorBravo.votingPeriod() + 1);
+    //     // We increment the time based on the voting period
+    //     increaseBlockNumber(governorBravo.votingPeriod() + 1);
 
-        // Asserts that the proposal was successful (4)
-        assertEq(governorBravo.state(proposalID), 4);
+    //     // Asserts that the proposal was successful (4)
+    //     assertEq(governorBravo.state(proposalID), 4);
 
-        // Queues the proposal
-        governorBravo.queue(proposalID);
+    //     // Queues the proposal
+    //     governorBravo.queue(proposalID);
 
-        // Wait the timelock delay
-        increaseBlockTimestamp(172800 + 1); // 172800 is the Timelock delay 0x6d903f6003cca6255D85CcA4D3B5E5146dC33925
+    //     // Wait the timelock delay
+    //     increaseBlockTimestamp(172800 + 1); // 172800 is the Timelock delay 0x6d903f6003cca6255D85CcA4D3B5E5146dC33925
 
-        // Asserts that the proposal was queued (5)
-        assertEq(governorBravo.state(proposalID), 5);
+    //     // Asserts that the proposal was queued (5)
+    //     assertEq(governorBravo.state(proposalID), 5);
 
-        // Attacker executes the proposal
-        governorBravo.execute(proposalID);
+    //     // Attacker executes the proposal
+    //     governorBravo.execute(proposalID);
 
-        // Asserts that the proposal was executed (7)
-        assertEq(governorBravo.state(proposalID), 7);
+    //     // Asserts that the proposal was executed (7)
+    //     assertEq(governorBravo.state(proposalID), 7);
 
-        // Attacker accepts the admin role in the timelock
-        timelock.acceptAdmin();
+    //     // Attacker accepts the admin role in the timelock
+    //     timelock.acceptAdmin();
 
-        vm.stopPrank();
+    //     vm.stopPrank();
 
-        // Validates that the attacker is the new admin
-        assertEq(timelock.pendingAdmin(), address(0));
-        assertEq(timelock.admin(), attacker);
-    }
+    //     // Validates that the attacker is the new admin
+    //     assertEq(timelock.pendingAdmin(), address(0));
+    //     assertEq(timelock.admin(), attacker);
+    // }
 }
